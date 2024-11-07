f43ddde56de508f0065a15fb679dcdf7b7c91fdb
amusayevv <amusayev20487@ada.edu.az>
2024-11-07 20:28:27 +0400

    tying to connect OAUTH linkedin

--- content.js
+++ content.js
@@ -1,14 +1,15 @@
 window.addEventListener("load", () => {
     chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         if (message.action === "new-user") () => {
+            // h1 is Name and Surname of the user
             const userName = document.querySelector("h1");
-      
+            console.log(userName);
+            
             if (userName) {
               const name = userName.textContent.trim();
           
               // Send the extracted data to the popup
               chrome.runtime.sendMessage({ name, action: "name-sent" });
-              console.log("Name sent:", name);
             }
             else {
                 console.log("Name not found");
