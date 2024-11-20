c31db3a45754c32f0280898d8d0db27a37390872
amusayevv <amusayev20487@ada.edu.az>
2024-11-20 21:30:31 +0400

    soting data in chrome.storage api

--- connect.js
+++
@@ -1,7 +0,0 @@
-const connectButton = document.querySelector(".connect-button");
-
-connectButton.addEventListener("click", () => {
-    chrome.runtime.sendMessage({ action: "new-user" }, () => {
-        window.location.href = "popup.html";
-    });
-})
